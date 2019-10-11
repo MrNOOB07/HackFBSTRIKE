4308bac8941acbab8a163ea7f293af0c4fc5b93e
MrNOOB <56033162+MrNOOB07@users.noreply.github.com>
2019-10-11 16:38:50 +0700

    Update python.py

--- python.py
+++ python.py
@@ -1,16 +1,7 @@
-# HackFBSTRIKE
+#HackFBSTRIKE
 Helo Kali Ini Saya Akan Membagikan Script Hack Fb Dengan Metode Crack Dijamin Langsung Strike
 #Hello Anonymous Indonesia 
 #Author Mr.NOOB07 $white
 #Black Coder Rush
 #C1ONING CYBER ARMY
 #Contack : 0895xxxx $white
-
-pkg install update 
-pkg install upgrade
-pkg install python
-pkg install python2
-pkg install git
-git clone https://github.com/MrNOOB07/HackFBSTRIKE
-cd HackFBSTRIKE
-python2 FBSTRIKE.py
