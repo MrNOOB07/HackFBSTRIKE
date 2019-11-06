0a8fc5bbe270a16fc3d0fccbea0566cc3bb89ab5
MrNOOB <56033162+MrNOOB07@users.noreply.github.com>
2019-11-06 23:05:24 +0700

    Create login.py

---
+++ login.py
@@ -0,0 +1,10 @@
+user = raw_input("Username Nya Mastahku: ")
+import getpass
+
+sandi = getpass.getpass()
+
+if sandi == 'NOOB07' and user == 'Mr':
+    print ("Anda Berhasil Login!")
+else:
+
+    print ("Username Atau Password Salah!")
